239bcb633e7180da06fb7608b2f12c26d383f5cd
Amit Virdee <amit@blisteringdevelopers.com>
2013-12-31 02:04:09 +1100

    added vircurex.py and shotgunbot.py

---
+++ shotgunbot.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+# Vircurex Shotgun Bot
+# User inputs the currency to buy/sell against btc, the range of prices they wish to aim at, and how much of each currency to spend. 
+# The bot then creates an order for every santoshi in the range, creating buys below market price, and asks above. 
+# The API password in Vircurex settings must be the same for get_balance, create_order and release_order for this to work properly.
+# If TheCleaner option eventually gets added, the API password for read_orders and delete_order must also be the same as those prior.
+#
+# Hunterbunter
+# Dec 2013
+
+from vircurex import Vircurex
+from vircurex import ShotgunSecrets
+from vircurex import PlaceOrders
+
+import sys, os
+
+def Verification():
+	"""Get username and password from either user or user.ini"""
+	while (1):
+			try:
+				with open ("user.ini") as f: 	# check if user.ini file exists, if so, use it
+					data = f.read()
+					data = data.split("\n")					
+					username = data[0]
+					password = data[1]
+			except IOError:						# if it doesn't exist, or couldn't be read, delete it
+				if os.path.exists("user.ini"):
+					os.remove("user.ini")
+				username = raw_input("\nEnter Vircurex Username: ")	# get user input for username and password
+				password = raw_input("Enter API password: ")
+			
+			secrets = ShotgunSecrets(password)	# create secrets dictionary
+			try:
+				exchange = Vircurex(username, secrets)	# create exchange object
+				response = exchange.get_balance("btc")	# get btc balance to verify username/password
+				if response['status'] != 0:				# if it fails, tell the user why
+					print "\n"+response['statustxt']
+					if response['status'] == 8003 and os.path.exists("user.ini"): # and if it fails because of incorrect username/password, delete the user.ini file if it exists and go back to start
+						 os.remove("user.ini")
+				else:
+					btc_balance = float(response['availablebalance'])	# store btc balance					
+					if not os.path.exists("user.ini"):	# ask user if they want to store their username/password so they don't have to enter it again to run this script
+						answer = raw_input("\nDo you wish to store your username/password for future use? Y/N: ")
+						if answer == "Y" or answer == "y": 
+							try:
+								f = open("user.ini", 'w')
+								f.write(username+"\n"+password)	
+								print "\nWARNING: This file is not encrypted; store at your own risk.\nUsername and Password saved in user.ini. Delete this file if you wish to remove it."
+							except IOError:
+								print "Could not write file user.ini"
+					return exchange, btc_balance
+			except:
+				raise
+
+def TheCleaner():
+	"""Get a full list of all released orders, and cancel all of them"""
+	print "Yet to be implemented. For now, you can delete all easily enough from the vircurex order page."
+
+def ShotgunBot():
+	"""Get User details, check each is valid, 
+	then spam vircurex with orders.
+	"""
+	# First get user's username and API password - User must use the same password for all of the functions for this to work.
+	print "\nVircurex Shotgun Bot."
+	try:
+		secrets = {}
+		username = ""
+		password = ""
+		currency = ""
+		min_price = 0.0
+		max_price = 0.0
+		btc_at_risk = 0.0
+		currency_at_risk = 0.0
+		btc_balance = 0.0
+		currency_balance = 0.0
+		
+		# access and verify username/password
+		exchange, btc_balance = Verification()
+		print "\nAvailable BTC balance: %.8f btc" % btc_balance
+		
+		# Get currency details, check that they are valid
+		while (1):
+			currency = raw_input("\nEnter Currency Code you wish to trade against BTC: ")
+			try:
+				response = exchange.get_balance(currency)	# verify currency code by getting available balance of it
+				if response['status'] != 0:
+					print "\n"+response['statustxt']
+				else:
+					currency_balance = float(response['availablebalance'])
+					print "\nAvailable %s balance: %.8f %s" % (currency.upper(), currency_balance, currency.lower())
+					break
+			except:
+				raise
+		
+		# Get minimum and maximum prices
+		while (1):
+			print "\n0.00000001 btc = 1 Santoshi"
+			try:
+				min_price = int(raw_input("Enter Minimum price in Santoshis: "))
+				max_price = int(raw_input("Enter Maximum price in Santoshis: "))
+				if max_price > min_price:
+					segments = max_price - min_price + 1
+					if segments > 1000:
+						answer = raw_input("\nYour range in prices is very large (%d). Are you sure you want to create this many orders? Y/N: " % segments)
+						if answer == "y" or answer == "Y":							
+							break
+					else:
+						break
+				else:
+					print "\nThe maximum price must be greater than the minimum price."
+			except ValueError:
+				print "\nPrices must be whole integers. If you want 0.00010000 as the price, you need to enter 10000."
+			except:
+				raise
+		min_price = min_price / 100000000.0
+		max_price = max_price / 100000000.0
+		# Get BTC and DVC to put up for buy and sell orders
+		while (1):
+			try:
+				str1 = "\nEnter amount of BTC to put up (%.8f available, leave blank to use all of it): " % btc_balance
+				str2 = "Enter amount of %s to put up (%.8f available, leave blank to use all of it): " % (currency.upper(), currency_balance)
+				answer = raw_input(str1)
+				if answer == "":
+					btc_at_risk = btc_balance
+				else:
+					btc_at_risk = float(answer)
+				if btc_at_risk > btc_balance:
+					raise					
+				answer = raw_input(str2)
+				if answer == "":
+					currency_at_risk = currency_balance
+				else:
+					currency_at_risk = float(answer)
+				if currency_at_risk > currency_balance:
+					raise
+				break
+			except KeyboardInterrupt:
+				raise
+			except:
+				print "\nThe amounts must be floating point numbers (or whole numbers), and equal to or below the amount available."
+		
+		# Get current marking bid and ask
+		try:
+			market_bid = exchange.get_highest_bid(currency, "btc")
+			market_ask = exchange.get_lowest_ask(currency, "btc")
+			if market_bid['value'] != None and market_ask['value'] != None:
+				avg_market_price = (float(market_bid['value']) + float(market_ask['value'])) / 2
+			else:
+				print "\nCould not get market prices. Connection or Vircurex may be down."
+				raise Exception
+			# find upper and lower range
+			if avg_market_price > min_price:
+				if max_price < avg_market_price: # all of our orders are going to be bids
+					lower_range = (max_price - min_price)*100000001 + 1 # 1000000001 because of rounding error going into int later
+				else:
+					lower_range = (avg_market_price - min_price)*100000001 	# how many santoshis are going to be buy orders (not all of them)
+				btc_segments = btc_at_risk / lower_range	# the btc value of each segment
+			else:
+				lower_range = 0
+				btc_segments = 0
+			if avg_market_price < max_price:
+				if min_price > avg_market_price:
+					upper_range = (max_price - min_price)*100000001 + 1
+				else:
+					upper_range = (max_price - avg_market_price)*100000001				# how many santoshis are going to be sell orders
+				currency_segments = currency_at_risk / upper_range	# the currency value of each segment
+			else:
+				upper_range = 0
+				currency_segments = 0
+			
+			print "\nReport:\nThe average market price is: %.8f btc per 1 %s" % (avg_market_price, currency)
+			print "There will be %.0f buy orders worth %.8f btc each - MUST be above 0.0001 btc for vircurex to accept orders" % (lower_range, btc_segments)
+			print "There will be %.0f sell orders worth %.8f %s each" % (upper_range, currency_segments, currency)
+			answer = raw_input("\nProceed? Y/N: ")
+			if answer == "Y" or answer == "y":
+				PlaceOrders(exchange, "buy", currency, btc_segments, min_price, int(lower_range))
+				PlaceOrders(exchange, "sell", currency, currency_segments, max_price, int(upper_range))
+			
+		except:
+			raise
+	except KeyboardInterrupt:
+		exit(1)
+	except Exception as e:
+		print e
+
+def Entry():
+	"""Ask user whether they want to close all open and released orders, or create more orders"""
+	print "\nVircurex ShotgunBot and Cleaner.\nWhat would you like to do?\n1) Create a batch of orders.\n2) Close all open and released orders."
+	answer = raw_input("Command: ")
+	if answer == "1":
+		ShotgunBot()
+	elif answer == "2":
+		TheCleaner()	
+		
+if __name__ == "__main__":
+	#Entry()
+	ShotgunBot()
+
---
+++ vircurex.py
@@ -0,0 +1,125 @@
+# Vircurex.py
+# This is a modified class based on https://github.com/alexroat/python-vircurex/blob/master/vircurex.py to support the shotgun bot.
+#
+# Hunterbunter
+# Dec 2013
+
+import time
+import json
+import urllib, urllib2
+import hashlib
+import random
+import sys
+
+class Vircurex:
+		domain="https://vircurex.com"#domain
+		@staticmethod
+		def simpleRequest(command,**params):
+				global domain
+				url="%s/api/%s.json?%s"%(Vircurex.domain,command,urllib.urlencode(params.items()))#url
+				request = urllib2.Request(url, headers={'User-Agent':"Magic Browser"})
+				con = urllib2.urlopen(request)
+				return json.load(con)
+		@staticmethod
+		def secureRequest(user,secret,command,tokenparams,**params):
+				global domain
+				t = time.strftime("%Y-%m-%dT%H:%M:%S",time.gmtime())#UTC time
+				txid=hashlib.sha256("%s-%f"%(t,random.randint(0,1<<31))).hexdigest();#unique trasmission ID using random hash
+                #token computation
+				vp=[command]+map(lambda x:params[x],tokenparams)
+				token_input="%s;%s;%s;%s;%s"%(secret,user,t,txid,';'.join(map(str,vp)))
+				token=hashlib.sha256(token_input).hexdigest()
+				#cbuilding request
+				reqp=[("account",user),("id",txid),("token",token),("timestamp",t)]+params.items()
+				url="%s/api/%s.json?%s"%(Vircurex.domain,command,urllib.urlencode(reqp))#url
+				request = urllib2.Request(url, headers={'User-Agent':"MagicBrowser"})
+				con = urllib2.urlopen(request)
+				return json.load(con)				
+		#insert user and a dict with secrets set in your account settings (e.g. : create_order=>q12we34r5t)
+		def __init__(self,user=None,secrets={}):
+				self.user=user
+				self.secrets=secrets
+		#trade API
+		def get_balance(self,currency):
+				return Vircurex.secureRequest(self.user,self.secrets["get_balance"],"get_balance",["currency"],currency=currency)
+		def get_balances(self):
+				return Vircurex.secureRequest(self.user,self.secrets["get_balance"],"get_balances",[])['balances']
+		def create_order(self,ordertype,amount,currency1,unitprice,currency2):
+				return Vircurex.secureRequest(self.user,self.secrets["create_order"],"create_order",["ordertype","amount","currency1","unitprice","currency2"],ordertype=ordertype,amount=amount,currency1=currency1,unitprice=unitprice,currency2=currency2)
+		def release_order(self,orderid):
+				return Vircurex.secureRequest(self.user,self.secrets["release_order"],"release_order",["orderid"],orderid=orderid)                
+		def delete_order(self,orderid,otype):
+				return Vircurex.secureRequest(self.user,self.secrets["delete_order"],"delete_order",["orderid","otype"],orderid=orderid,otype=otype)
+		def read_order(self,orderid,otype):
+				return Vircurex.secureRequest(self.user,self.secrets["read_order"],"read_order",["orderid"],orderid=orderid,otype=otype)
+		def read_orders(self,otype):
+				return Vircurex.secureRequest(self.user,self.secrets["read_orders"],"read_orders",[],otype=otype)
+		def read_orderexecutions(self,orderid):
+				return Vircurex.secureRequest(self.user,self.secrets["read_orderexecutions"],"read_orderexecutions",["orderid"],orderid=orderid)
+		def create_coupon(self,amount,currency):
+				return Vircurex.secureRequest(self.user,self.secrets["create_coupon"],"create_coupon",["amount","currency"],amount=amount,currency=currency)
+		def redeem_coupon(self,coupon):
+				return Vircurex.secureRequest(self.user,self.secrets["redeem_coupon"],"redeem_coupon",["coupon"],coupon=coupon)
+		##info API
+		def get_lowest_ask(self,base,alt):
+				return Vircurex.simpleRequest("get_lowest_ask",base=base,alt=alt)
+		def get_highest_bid(self,base,alt):
+				return Vircurex.simpleRequest("get_highest_bid",base=base,alt=alt)
+		def get_last_trade(self,base,alt):
+				return Vircurex.simpleRequest("get_last_trade",base=base,alt=alt)
+		def get_volume(self,base,alt):
+				return Vircurex.simpleRequest("get_volume",base=base,alt=alt)
+		def get_info_for_currency(self):
+				return Vircurex.simpleRequest("get_info_for_currency")
+		def get_info_for_1_currency(self,base,alt):
+				return Vircurex.simpleRequest("get_info_for_1_currency",base=base,alt=alt)
+		def orderbook(self,base,alt):
+				return Vircurex.simpleRequest("orderbook",base=base,alt=alt)
+		def orderbook_alt(self,alt):
+				return Vircurex.simpleRequest("orderbook_alt",alt=alt)
+		def trades(self,base,alt,since):
+				return Vircurex.simpleRequest("trades",base=base,alt=alt,since=since)
+		def get_currency_info(self):
+				return Vircurex.simpleRequest("get_currency_info")
+
+def ShotgunSecrets(secret):
+	secrets = {}
+	secrets['get_balance'] = secret
+	secrets['create_order'] = secret
+	secrets['release_order'] = secret
+	secrets['delete_order'] = secret
+	secrets['read_order'] = secret
+	secrets['read_orders'] = secret
+	secrets['read_orderexecution'] = secret
+	secrets['create_coupon'] = secret
+	secrets['redeem_coupon'] = secret
+	return secrets
+
+def PlaceOrders(exchange, type, currency, segments, price, santoshis):
+	if type == "buy":
+		print("\nPlacing %d Buy Orders:" % santoshis)
+	else:
+		print("\nPlacing %d Sell Orders:" % santoshis)
+	count = 0
+	for i in range(santoshis):
+		if type == "buy":
+			qty = segments / price				
+		else:
+			qty = segments
+		str = "\nOrder: %s %.8f %s @ %.8f btc..." % (type.capitalize(), qty, currency, price)
+		sys.stdout.write(str)
+		response = exchange.create_order(type, qty, currency, price, "btc")
+		if response['status'] == 0:
+			response = exchange.release_order(response['orderid'])
+			if response['status'] == 0:
+				sys.stdout.write("Order Released")
+				count += 1
+			else:
+				print "Order failed to release: %s" % response['statustxt']
+		else:
+			print "Order failed to release: %s" % response['statustxt']
+		if type == "buy":
+			price += 0.00000001
+		else:
+			price -= 0.00000001
+	print("\n%s Orders Complete: %d orders placed." % (type.capitalize(), count))
